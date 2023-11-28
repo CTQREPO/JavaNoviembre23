bab8891efb0e65aaedd0452775951dec74818051
gabrielarobledo03 <gabrielarobledo_@hotmail.com>
2023-11-28 08:48:59 -0600

    Practica de clases y objetos GRR

---
+++ objetos_grr/src/com/Reloj.java
@@ -0,0 +1,88 @@
+package com;
+
+public class Reloj {
+
+	// Atributos EL QUE
+	String color;
+	String tipo;
+	String materiales;
+
+	int horas, minutos;
+	// contructores
+
+	public Reloj() {
+	}
+
+	public Reloj(String materiales, int horas, int minutos) { // argumentos
+		super();
+		this.materiales = materiales;
+		this.horas = horas;
+		this.minutos = minutos;
+	}
+	
+	
+	
+	
+	public Reloj(String color, String tipo, String materiales, int horas, int minutos) { // argumentos
+		super();
+		this.color = color;
+		this.tipo = tipo;
+		this.materiales = materiales;
+		this.horas = horas;
+		this.minutos = minutos;
+	}
+	
+	//Getters & Setters
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) { //(devuelven vacios) los public void para generarle datos al objeto
+		this.color = color;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getMateriales() {
+		return materiales;
+	}
+
+	public void setMateriales(String materiales) {
+		this.materiales = materiales;
+	}
+
+	public int getHoras() { //estos public int, public string devuelven algo
+		return horas;
+	}
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+	
+	
+
+	// toString
+	@Override
+	public String toString() {
+		return "Reloj [color=" + color + ", tipo=" + tipo + ", materiales=" + materiales + ", horas=" + horas
+				+ ", minutos=" + minutos + "]";
+	}
+
+	
+}
