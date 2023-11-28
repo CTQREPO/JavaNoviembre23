ab0715f6945ac75096bdb1b55b81eb6ea09c40e4
josegerardorb <ramirezbernal.josegerardo@gmail.com>
2023-11-28 08:49:37 -0600

    practica de clases y objetos jgrb

---
+++ objetos_jgrb/src/com/Reloj.java
@@ -0,0 +1,67 @@
+package com;
+
+public class Reloj {
+	
+	//Atributos QUE
+	String color;
+    String tipo;
+    String materiales;
+    
+    int horas, minutos;
+    
+	//Constructores
+    public Reloj () {
+	
+}
+	public Reloj(String color, String tipo, String materiales, int horas, int minutos) {
+		super();
+		this.color = color;
+		this.tipo = tipo;
+		this.materiales = materiales;
+		this.horas = horas;
+		this.minutos = minutos;
+	}
+	@Override
+	public String toString() {
+		return "Reloj [color=" + color + ", tipo=" + tipo + ", materiales=" + materiales + ", horas=" + horas
+				+ ", minutos=" + minutos + "]";
+	}
+	
+	
+	//Getters & Setters
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getMateriales() {
+		return materiales;
+	}
+	public void setMateriales(String materiales) {
+		this.materiales = materiales;
+	}
+	public int getHoras() {
+		return horas;
+	}
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+	public int getMinutos() {
+		return minutos;
+	}
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+	
+	
+	
+	//toString
+}
