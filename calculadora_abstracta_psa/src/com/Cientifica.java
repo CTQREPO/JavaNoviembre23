706f9baeffee6892f5f566a2e91812629328b555
pablo23bruce <psantillanalvarez@gmail.com>
2023-11-30 08:39:53 -0600

    TERMINADA

--- calculadora_abstracta_psa/src/com/Cientifica.java
+++ calculadora_abstracta_psa/src/com/Cientifica.java
@@ -15,6 +15,8 @@ public class Cientifica extends Calculadora_Abstracta {
 		System.out.println("Digite el primer número.");
 		b = numB.nextDouble();
 
+		resultado = a + b;
+
 		System.out.println("El resultado de la suma es " + (a + b));
 
 	}
@@ -23,17 +25,53 @@ public class Cientifica extends Calculadora_Abstracta {
 	public void restaNums() {
 		// TODO Auto-generated method stub
 
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		Scanner numB = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		b = numB.nextDouble();
+
+		resultado = a - b;
+
+		System.out.println("El resultado de la resta es " + (a - b));
+
 	}
 
 	@Override
 	public void multiNums() {
 		// TODO Auto-generated method stub
 
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		Scanner numB = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		b = numB.nextDouble();
+
+		resultado = a * b;
+
+		System.out.println("El resultado de la multiplicación es " + (a * b));
+
 	}
 
 	@Override
 	public void divideNums() {
 		// TODO Auto-generated method stub
+
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		Scanner numB = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		b = numB.nextDouble();
+
+		resultado = a / b;
+
+		System.out.println("El resultado de la división es " + (a / b));
 
 	}
 
@@ -50,6 +88,66 @@ public class Cientifica extends Calculadora_Abstracta {
 		resultado = Math.pow(a, b);
 
 		System.out.println(a + " elevado a la " + b + " potencia es " + (Math.pow(a, b)));
+
+	}
+
+	public void calculaSeno() {
+
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		resultado = Math.sin(a);
+
+		System.out.println("El seno de " + a + " es " + resultado);
+
+	}
+
+	public void calculaCos() {
+
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		resultado = Math.cos(a);
+
+		System.out.println("El coseno de " + a + " es " + resultado);
+
+	}
+
+	public void calculaTan() {
+
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		resultado = Math.tan(a);
+
+		System.out.println("La tangente de " + a + " es " + resultado);
+
+	}
+
+	public void calculaArcsen() {
+
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		resultado = Math.asin(a);
+
+		System.out.println("El arco seno de " + a + " es " + resultado);
+
+	}
+
+	public void calculaArccos() {
+
+		Scanner numA = new Scanner(System.in);
+		System.out.println("Digite el primer número.");
+		a = numA.nextDouble();
+
+		resultado = Math.acos(a);
+
+		System.out.println("El arco coseno de " + a + " es " + resultado);
 
 	}
 
